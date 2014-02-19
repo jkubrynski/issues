8ace973717cfb378839f68de0c7b44c538dff67e
Jakub Kubrynski <jkubrynski@gmail.com>
2014-02-20 00:06:41 +0100

    IDEA-119569

---
+++ IDEA-119569/src/main/java/com/proof/SpringBean.java
@@ -0,0 +1,11 @@
+package com.proof;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jkubrynski@gmail.com / 2014-02-19
+ */
+@Component
+public class SpringBean {
+
+}
---
+++ IDEA-119569/src/main/java/com/proof/SpringConfig.java
@@ -0,0 +1,13 @@
+package com.proof;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by jkubrynski@gmail.com / 2014-02-19
+ */
+@Configuration
+@ComponentScan
+public class SpringConfig {
+
+}
